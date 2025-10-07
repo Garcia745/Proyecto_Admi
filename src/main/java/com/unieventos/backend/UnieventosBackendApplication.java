cdc8bd7028bd0ae86d76ec261fa0249518d4f9f3
Garcia745 <129553669+Garcia745@users.noreply.github.com>
2025-10-06 19:00:30 -0500

    Subiendo proyecto base Spring Boot

---
+++ src/main/java/com/unieventos/backend/UnieventosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unieventos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnieventosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnieventosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unieventos/backend/UnieventosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unieventos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnieventosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
