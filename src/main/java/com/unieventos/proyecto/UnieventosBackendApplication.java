96beadf4843c887e0005c3ef3722b6036028fec9
Garcia745 <129553669+Garcia745@users.noreply.github.com>
2025-10-29 13:30:53 -0500

    Configuracion del gradle

--- src/main/java/com/unieventos/proyecto/UnieventosBackendApplication.java
+++ src/main/java/com/unieventos/proyecto/UnieventosBackendApplication.java
@@ -1,4 +1,4 @@
-package com.unieventos.backend;
+package com.unieventos.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnieventosBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UnieventosBackendApplication.class, args);
+
+        SpringApplication.run(UnieventosBackendApplication.class, args);
 	}
 
 }
--- src/test/java/com/unieventos/proyecto/UnieventosBackendApplicationTests.java
+++ src/test/java/com/unieventos/proyecto/UnieventosBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.unieventos.backend;
+package com.unieventos.proyecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
