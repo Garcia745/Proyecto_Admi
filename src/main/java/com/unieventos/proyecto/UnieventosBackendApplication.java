930684faca7856b32fd89e373da8473081790002
Garcia745 <129553669+Garcia745@users.noreply.github.com>
2025-10-29 13:37:48 -0500

    prueba

--- src/main/java/com/unieventos/proyecto/UnieventosBackendApplication.java
+++ src/main/java/com/unieventos/proyecto/UnieventosBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnieventosBackendApplication {
 
 	public static void main(String[] args) {
-
+//prueba
         SpringApplication.run(UnieventosBackendApplication.class, args);
 	}
 
