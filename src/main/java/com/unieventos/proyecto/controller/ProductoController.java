49f7c0b6ecfa6834b21732fad1fbfedc577e7db6
juanDa-dev373 <juand.riosf1@uqvirtual.edu.co>
2025-11-05 23:53:38 -0500

    Se actualizan clase

--- src/main/java/com/unieventos/proyecto/controller/ProductoController.java
+++ src/main/java/com/unieventos/proyecto/controller/ProductoController.java
@@ -1,17 +1,25 @@
 package com.unieventos.proyecto.controller;
 
+import com.unieventos.proyecto.model.Producto;
+import com.unieventos.proyecto.service.Interface.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/producto/")
 public class ProductoController {
 
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping
-    public ResponseEntity<String> getAllProductos() throws Exception{
-        return ResponseEntity.ok().body("[]");
+    public ResponseEntity<List<Producto>> getAllProductos() {
+        return ResponseEntity.ok().body(productoService.getAllProducts());
     }
 
 
--- src/main/java/com/unieventos/proyecto/service/Implementation/ProductoServiceImpl.java
+++ src/main/java/com/unieventos/proyecto/service/Implementation/ProductoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.unieventos.proyecto.service.Implementation;
 
 import com.unieventos.proyecto.model.Producto;
+import com.unieventos.proyecto.repository.repository.ProductoRepo;
 import com.unieventos.proyecto.service.Interface.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +11,11 @@ import java.util.List;
 @Service
 public class ProductoServiceImpl implements ProductoService {
 
+    @Autowired
+    private ProductoRepo repository;
 
     @Override
     public List<Producto> getAllProducts() {
-        return List.of();
+        return repository.findAll();
     }
 }
